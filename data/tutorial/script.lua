c96ae6c6d56cbbc89d185aebd14d384caa7e3354
Ledonic <ledonic7@gmail.com>
2025-08-16 13:52:07 +0400

    Make changes according to the Lumen source code

--- data/tutorial/script.lua
+++ data/tutorial/script.lua
@@ -1,5 +1,6 @@
 -- Just a little script to put GF where she's meant to be.
 function onCreate()
+    setProperty('gf.visible', false)
     setProperty('dad.x', getProperty('gf.x'))
     setProperty('dad.y', getProperty('gf.y'))
 end
