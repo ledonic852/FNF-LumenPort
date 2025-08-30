b87e26405a37424f23a963fd7025c8db86c83abc
Ledonic <ledonic7@gmail.com>
2025-08-30 15:06:00 +0400

    Change Pico's position for cutscenes

--- characters/pico-speaker.lua
+++ characters/pico-speaker.lua
@@ -1,4 +1,4 @@
 function onCreatePost()
     addLuaScript('characters/props/speaker')
-    callScript('characters/props/speaker', 'createSpeaker', {'pico-speaker', -190, 438}) -- {characterName, offsetX, offsetY}
+    callScript('characters/props/speaker', 'createSpeaker', {'pico-speaker', -215, 438}) -- {characterName, offsetX, offsetY}
 end
